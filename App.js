9bd1abd58c4c0e28a00d7ffb0ada2145bbbd99a8
Yusuf Ibrahim <yusuf.ibrahim@harukaedu.com>
2018-06-21 11:19:53 +0700

    stylesheet width basic

--- App.js
+++ App.js
@@ -1,88 +1,17 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View, Image as Gambar, StyleSheet } from 'react-native';
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {isShowingText: true};
-
-    // Toggle the state every second
-    setInterval(() => {
-      this.setState(previousState => {
-        return { isShowingText: !previousState.isShowingText };
-      });
-    }, 1000);
-  }
-
-  render() {
-    let display = this.state.isShowingText ? this.props.text : ' ';
-    return (
-      <Text style={gayamu.bigblue}>{display}</Text>
-    );
-  }
-}
-
-class Psg extends Component {
+export default class FixedDimensionsBasics extends Component {
   render() {
     return (
-      <Gambar source={this.props.pic} style={{width: 193, height: 110}}/>
-    );
-  }
-}
-
-class Greeting extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      firstInput: 1,
-    }
-
-    setInterval(() => {
-      this.setState(previousState => {
-        return { firstInput: this.state.firstInput+=1 }
-      });
-    }, 1000);
-  }
-
-  render() {
-    name = this.props.name + " Xioumi " + this.state.firstInput;
-    return (
-      <Text style={gayamu.red}>Hellou {name} {this.props.age} !</Text>
-    );
-  }
-}
-
-export default class LotsOfGreetings extends Component {
-  render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
-
-    return (
-      <View style={{alignItems: 'center'}}>
-        <Greeting name='Rexxari' age='30'/>
-        <Greeting name='Jainai' age='32'/>
-        <Greeting name='Veraina' age='390'/>
-        <Psg pic={pic}/>
-        <Blink text='wakwauuuu'/>
+      <View>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
 }
-
-
-const gayamu = StyleSheet.create({
-  bigblue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-});
 
 // skip this line if using Create React Native App
 //AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
