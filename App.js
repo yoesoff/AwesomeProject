d2e487f31f9dbd138b24da3c812d91fe81ea775e
Yusuf Ibrahim <yusuf.ibrahim@harukaedu.com>
2018-06-21 11:20:27 +0700

    stylesheet Flex basic

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, Image as Gambar, StyleSheet } from 'react-native';
+import {View } from 'react-native';
 
 export default class FixedDimensionsBasics extends Component {
   render() {
     return (
-      <View>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      <View style={{flex: 1}}>
+        <View style={{flex: 2, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 2, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
