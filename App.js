2a35d5c032ec18f72bfe4be42c4ee1bdf4cc1c2d
Yusuf Ibrahim <yusuf.ibrahim@harukaedu.com>
2018-06-21 13:37:19 +0700

    Input text handling

--- App.js
+++ App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
-import {View } from 'react-native';
+import {Text, TextInput, View} from 'react-native';
 
-export default class FixedDimensionsBasics extends Component {
+class TextViewer extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {xxx: 'mulai !'}
+    }
+
+    render() {
+      return (
+        <View style={{padding: 10}}>
+          <TextInput
+            style={{height: 40}}
+            placeholder="Type here to translate!"
+            onChangeText={(xxx) => this.setState({xxx})}
+          />
+          <Text style={{padding: 10, fontSize: 42}}>
+            {this.state.xxx + '🍕'}
+          </Text>
+        </View>
+      );
+    }
+}
+
+export default class PizzaTranslator extends Component {
   render() {
     return (
-      <View style={{flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
-        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
-      </View>
+      <TextViewer />
     );
   }
 }
+
+
 
 // skip this line if using Create React Native App
 //AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);
