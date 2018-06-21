d2548ff3d2dc4e14152b52d91968f157f97fa12a
Yusuf Ibrahim <yusuf.ibrahim@harukaedu.com>
2018-06-21 11:25:12 +0700

    stylesheet basic Flex layouting with flexDirection and justifyContent

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ import {View } from 'react-native';
 export default class FixedDimensionsBasics extends Component {
   render() {
     return (
-      <View style={{flex: 1}}>
-        <View style={{flex: 2, backgroundColor: 'powderblue'}} />
-        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-        <View style={{flex: 2, backgroundColor: 'steelblue'}} />
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+        <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
       </View>
     );
   }
